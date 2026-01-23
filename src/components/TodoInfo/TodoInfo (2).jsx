37efa5935fe5abee29be0d78993dda95e6f07613
Anastasiia Levochkina <nastya.mixaylova98@gmail.com>
2026-01-23 02:49:20 +0200

    Fix project structure (remove nested folder)

---
+++ src/components/TodoInfo/TodoInfo (2).jsx
@@ -0,0 +1,14 @@
+import { UserInfo } from '../UserInfo';
+
+// Add the required props
+export const TodoInfo = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  return (
+    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
---
+++ src/components/UserInfo/UserInfo (2).jsx
@@ -0,0 +1,10 @@
+// Add the required props
+export const UserInfo = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
